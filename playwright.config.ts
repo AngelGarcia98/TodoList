ac629ebb84f5470a3619cd2780b2c018f3a31bbd
Angel <agga0405@gmail.com>
2026-04-25 17:26:42 -0600

    fix: unificar todo al puerto 3000

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,12 +8,21 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
+
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], launchOptions: { slowMo: 800 } },
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
+
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('Crear tarea', async ({ page }) => {
-  await page.goto('http://localhost:3004/');
-  await page.getByPlaceholder('Agregar nueva tarea...').type('Comprar leche', { delay: 100 });
-  const button=page.getByRole('button', {name:'Agregar'})
+  // Entramos al puerto 3000
+  await page.goto('http://localhost:3000/');
+  
+  // Tu test de la ToDo List
+  await page.getByPlaceholder('Agregar nueva tarea...').fill('Comprar leche');
+  const button = page.getByRole('button', { name: 'Agregar' });
+  
   await expect(button).toBeEnabled();
-  await button.click()
-  await expect(page.getByText('Comprar leche')).toBeVisible();
+  await button.click();
 });
 
 
